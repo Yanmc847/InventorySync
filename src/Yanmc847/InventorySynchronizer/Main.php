71ab63a571d128a1ba41a994bbb263758dad3948
Yanmc847 <Yanmc847@gmail.com>
2022-11-12 00:18:09 +0100

    Initial Commit

---
+++ src/Yanmc847/InventorySynchronizer/Main.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Yanmc847\InventorySync;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\event\{
+	Listener,
+	player\PlayerLoginEvent,
+	player\PlayerQuitEvent
+};
+
+class Main extends PluginBase implements Listener {
+
+	private $config;
+	private static $instance;
+
+	public function onLoad() : void
+	{
+		self::$instance = $this;
+	}
+
+	public function onEnable() : void
+	{
+		@mkdir($this->getDataFolder());
+		if(!file_exists($this->getDataFolder()."config.yml")) $this->saveResource("config.yml");
+		$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		Database::init();
+	}
+
+	public function onDisable(): void
+    {
+        foreach ($this->getServer()->getOnlinePlayers() as $player) {
+            if(Database::isRegistered($player)){
+                Database::saveInventory($player);
+            }
+        }
+    }
+
+    public function onLogin(PlayerLoginEvent $ev)
+	{
+		$player = $ev->getPlayer();
+
+		if(!Database::isRegistered($player)){
+			Database::register($player);
+		} else {
+			Database::restoreInventory($player);
+		}
+	}
+
+	public function onQuit(PlayerQuitEvent $ev)
+	{
+		$player = $ev->getPlayer();
+
+		if(Database::isRegistered($player)){
+			Database::saveInventory($player);
+		}
+	}
+
+	public function getConfig() : Config
+	{
+		return $this->config;
+	}
+
+	public static function getInstance()
+	{
+		return self::$instance;
+	}
+}
